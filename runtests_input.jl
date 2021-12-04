0f67bfd5a5a7946071733bd71a764dd4ce6f9c00
Sai-Chandar <n.saichander@gmail.com>
2021-12-04 16:58:49 -0500

    updating test cases

--- runtests_input.jl
+++ runtests_input.jl
@@ -19,8 +19,8 @@ include("read_input.jl")
         @test size(w) == (100,)
     end
 
-    @testset "file 100_25_2" begin
-        f = "./data/r_100_25_4.txt"
+    @testset "file 100_75_4" begin
+        f = "./data/r_100_75_4.txt"
         n, c, w = input(f)
         @test n == 100
         @test size(c) == (100, 100)
@@ -35,16 +35,16 @@ include("read_input.jl")
         @test size(w) == (100,)
     end
 
-    @testset "file 200_25_1" begin
-        f = "./data/r_200_25_1.txt"
+    @testset "file 200_25_7" begin
+        f = "./data/r_200_25_7.txt"
         n, c, w = input(f)
         @test n == 200
         @test size(c) == (200, 200)
         @test size(w) == (200,)
     end
 
-    @testset "file 300_25_1" begin
-        f = "./data/r_300_25_1.txt"
+    @testset "file 300_25_4" begin
+        f = "./data/r_300_25_4.txt"
         n, c, w = input(f)
         @test n == 300
         @test size(c) == (300, 300)
